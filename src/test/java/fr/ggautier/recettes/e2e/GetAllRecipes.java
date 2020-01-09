9f91bb02674f3378467b7eee42f93b75d8ed5b96
JesusPatate <gg.jesuspatate@orange.fr>
2020-01-09 21:33:58 +0100

    Replaced repository by entity manager in GetAllRecipes E2E test

--- src/test/java/fr/ggautier/recettes/e2e/GetAllRecipes.java
+++ src/test/java/fr/ggautier/recettes/e2e/GetAllRecipes.java
@@ -1,9 +1,10 @@
 package fr.ggautier.recettes.e2e;
 
-import fr.ggautier.recettes.domain.Recipe;
-import fr.ggautier.recettes.domain.Recipes;
+import fr.ggautier.recettes.spi.RecipeDbModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
 @AutoConfigureMockMvc
+@AutoConfigureTestEntityManager
 @Transactional
 class GetAllRecipes {
 
@@ -28,13 +30,13 @@ class GetAllRecipes {
     private MockMvc mvc;
 
     @Autowired
-    private Recipes repository;
+    private TestEntityManager entityManager;
 
     @Test
     void testGetAll() throws Exception {
         // Given
-        final Recipe recipe1 = new Recipe(UUID.randomUUID(), "recipe1");
-        final Recipe recipe2 = new Recipe(UUID.randomUUID(), "recipe2");
+        final RecipeDbModel recipe1 = new RecipeDbModel(UUID.randomUUID(), "recipe1");
+        final RecipeDbModel recipe2 = new RecipeDbModel(UUID.randomUUID(), "recipe2");
         this.storeRecipes(recipe1, recipe2);
 
         // When
@@ -48,7 +50,8 @@ class GetAllRecipes {
             .andExpect(jsonPath("$[1].title").value(recipe2.getTitle()));
     }
 
-    private void storeRecipes(final Recipe... recipes) {
-        Arrays.stream(recipes).forEach(recipe -> this.repository.save(recipe));
+    private void storeRecipes(final RecipeDbModel... recipes) {
+        Arrays.stream(recipes).forEach(this.entityManager::persist);
+        this.entityManager.flush();
     }
 }
