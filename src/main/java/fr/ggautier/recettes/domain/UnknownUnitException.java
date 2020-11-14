15d33c5b92f596712817b4a1896ddbc6c1bba60b
JesusPatate <gg.jesuspatate@orange.fr>
2020-11-14 21:50:33 +0100

    Fix : UnknownUnitException not handled correctly

--- src/main/java/fr/ggautier/recettes/domain/UnknownUnitException.java
+++ src/main/java/fr/ggautier/recettes/domain/UnknownUnitException.java
@@ -1,8 +1,14 @@
 package fr.ggautier.recettes.domain;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class UnknownUnitException extends Exception {
 
-    UnknownUnitException(final String unit) {
+    private static final String MESSAGE = "Unknown unit : %s";
 
+    UnknownUnitException(final String unit) {
+        super(String.format(MESSAGE, unit));
     }
 }
